225f7627acc805694e3cc3f1f0110fcf49a05619
Angelo Marasa <angelo.marasa@kaleidico.com>
2026-03-20 11:25:33 -0400

    Remove header and footer from main layout - landing pages only

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,21 +1,7 @@
-import { SiteHeader } from "@/components/layout/site-header";
-import { SiteFooter } from "@/components/layout/site-footer";
-import { BackToTop } from "@/components/layout/back-to-top";
-import { getSettings } from "@/lib/sanity/queries/settings";
-
-export default async function MainLayout({
+export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const settings = await getSettings();
-
-  return (
-    <>
-      <SiteHeader settings={settings} />
-      <main>{children}</main>
-      <SiteFooter settings={settings} />
-      <BackToTop />
-    </>
-  );
+  return <>{children}</>;
 }
